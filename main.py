fc4a250f82d8a76abe2be0870a17ba3cf9b041ae
thomasbreydo <tbreydo@gmail.com>
2019-08-23 16:41:48 -0400

    Add compare method to Code class

--- main.py
+++ main.py
@@ -3,7 +3,11 @@
 import mastermind
 
 def main():
-	pass
+	secret_code = mastermind.Code(*'b a c c'.split())
+	guess = mastermind.Code(*'a b c b'.split())
+
+	print(secret_code.compare(guess))
+
 
 if __name__ == '__main__':
 	main()
