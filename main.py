404e671bd95792999ebd8c53bcad5839a2fee65d
thomasbreydo <tbreydo@gmail.com>
2019-08-23 20:29:33 -0400

    TODO: - add minmax algorithm functionality - add docstrings - add back functionality to main.py

--- main.py
+++ main.py
@@ -3,15 +3,12 @@
 import mastermind
 
 def main():
-	game = mastermind.Game(5, 'a b c d e f g h i j k l'.split())
+	game = mastermind.Game()
 	while len(game.possibilities) > 1:
 		print(f'\nMy guess is {game.guess}.')
 		b = int(input('How many black pegs?\n> '))
 		w = int(input('How many white pegs?\n> '))
 		game.trim((b, w), 'rndm')
-		print(f'DEBUG:root: remaining possibilities: {len(game.possibilities)}')
-
-	ADD BACK FUNCTIONALITY
 
 	print(f'Your code was {game.guess}')
 
