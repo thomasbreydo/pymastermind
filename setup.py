ee77a83e844f72d503c616ca66ade0aa39147fcd
thomasbreydo <tbreydo@gmail.com>
2019-08-31 16:46:49 -0400

    Restructure + setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+with open('README.md') as f:
+	description = f.read()
+
+setup(
+   name='mastermind',
+   version='1.0',
+   description=description,
+   author='Thomas Breydo',
+   author_email='tbreydo@gmail.com',
+   packages=['mastermind'],
+   install_requires=['pandas']
+)
