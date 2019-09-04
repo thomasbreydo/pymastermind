cf86b06883f3b8c08b297b8f0d5555cf0996b367
thomasbreydo <tbreydo@gmail.com>
2019-09-04 10:55:20 -0400

    + short description and try to get setup to work

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.md') as f:
-    description = f.read()
+    long_description = f.read()
 
 setup(
    name='mastermind',
-   version='1.0',
-   description=description,
-   url='https://github.com/thomasbreydo/mastermind',
+   version='1',
    author='Thomas Breydo',
    author_email='tbreydo@gmail.com',
-   packages=['mastermind'],
+   url='https://github.com/thomasbreydo/mastermind',
+   description='A Python package for simulation of the MasterMind game.',
+   long_description=long_description,
+   license='MIT',
+   packages=find_packages(),
    install_requires=['pandas', 'tqdm'],
-   include_package_data=True,
+   # include_package_data=True,
 )
